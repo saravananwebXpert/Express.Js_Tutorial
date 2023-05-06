e9b997cd3428491e1c4a304a98ec886d2a8989e4
saravananm292 <saravananm292@gmail.com>
2023-05-06 15:51:03 +0530

    calling api with id

--- index.js
+++ index.js
@@ -8,3 +8,27 @@ const port = 3000;
 
 app.get("/", (req, res) => res.send("Maanavan.com"));
 app.listen(port, () => console.log("server is running"));
+
+var products = [
+  {
+    id: 1,
+    name: "IPhone",
+    price: 1000,
+  },
+  {
+    id: 2,
+    name: "Mac book pro",
+    price: 5000,
+  },
+];
+
+var product = [
+    {
+      id: 1,
+      name: "IPhone",
+      price: 1000,
+    }
+  ];
+
+app.get("/products", (req, res) => res.send(products));
+app.get("/products/1", (req, res) => res.send(product));
